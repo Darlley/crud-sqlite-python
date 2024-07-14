7f8a02be207e236d655e4c9807a2583eadd7ff24
Darlley <systembrtt@gmail.com>
2024-07-14 19:55:37 -0400

    initial commit

--- use.py
+++ use.py
@@ -1,13 +1,16 @@
 from dml import db_insert, db_delete, db_updateName, db_updatePhone, db_updateEmail, db_select
 from pprint import pprint
 
-# db_delete(1)
-# db_delete(3)
+# db_delete(4)
+# db_delete(5)
+# db_delete(6)
+# db_delete(7)
+# db_delete(8)
 
-# db_insert('fulano', '5567993328678', 'fulano@mail.com')
-# db_insert('ciclano', '5567993328678', 'ciclano@mail.com')
-# db_insert('beltrano', '5567993328678', 'beltrano@mail.com')
-# db_insert('Jane Doe', '5567993328678', 'jane_doe@mail.com')
-# db_insert('John Doe', '5567993328678', 'john_doe@mail.com')
+db_insert('fulano', '5567999999999', 'fulano@mail.com')
+db_insert('ciclano', '5567999999999', 'ciclano@mail.com')
+db_insert('beltrano', '5567999999999', 'beltrano@mail.com')
+db_insert('Jane Doe', '5567999999999', 'jane_doe@mail.com')
+db_insert('John Doe', '5567999999999', 'john_doe@mail.com')
 
-pprint(db_select('5567993328678', 'phone'))
+# pprint(db_select('5567999999999', 'phone'))
